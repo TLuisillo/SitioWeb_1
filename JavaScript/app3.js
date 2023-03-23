879c72a61eb545f4f45564f6e16e7070263f5f2e
TLuisillo <lcml.hormiga@gmail.com>
2023-03-23 02:46:32 -0600

    segundo commit JS

---
+++ JavaScript/app3.js
@@ -0,0 +1,15 @@
+// MANIPULACION DOM - LOS ELEMENTOS
+
+const abuelo = document.querySelector('ul.lista-cursos > li.list');
+const hermano = document.querySelector('ul.lista-cursos > li.list');
+
+
+
+//ELEMENTOS HIJO
+
+//console.log(padre.children[0]);
+//console.log(padre.firstElementChild);
+
+
+console.log(abuelo.parentElement.parentElement);
+console.log(hermano.nextElementSibling);
---
+++ JavaScript/app4.js
@@ -0,0 +1,12 @@
+// NODOS PARTE 1
+
+
+let elemento = document.getElementById('cursos');
+//let elemento = document.getElementsByTagName('nombreEtiqueta');
+//let elemento = document.getElementsByClassName('nombreClase');
+
+//let asd = document.querySelector('.nombreClase ó #idSelector')
+
+
+console.log(elemento);
+
